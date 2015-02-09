aed32f742fdb8af3ee5f41f4f523fd86f23edfa7
Chris. Ahn <cgpink@hanmail.net>
2015-02-09 14:28:50 +0900

    Split 메소드 학습 테스트 추가

--- src/string/StringTest.java
+++ src/string/StringTest.java
@@ -7,13 +7,16 @@ import org.junit.Test;
 
 public class StringTest {
 
+		
+	String testString = "안경찬은 천재인가, 바보인가";
+	
 	@Test
 	public void testSubString() {
-		
-		String testString = "안경찬은 천재인가, 바보인가";
-	
-		assertThat(testString.substring(0, 2), is("안경") );
-		
+		assertThat(testString.substring(0, 2), is("안경") );		
 	}
 	
+	@Test
+	public void testSplit() {
+		assertThat(testString.split(",")[0], is("안경찬은 천재인가") );
+	}
 }
