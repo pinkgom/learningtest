a830a58a6e40c6119457cbe3f63f961cfecd9cf0
Chris. Ahn <cgpink@hanmail.net>
2015-02-09 13:57:54 +0900

    Substring 학습 테스트 추가

--- src/string/StringTest.java
+++ src/string/StringTest.java
@@ -1,5 +1,19 @@
 package string;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class StringTest {
 
+	@Test
+	public void testSubString() {
+		
+		String testString = "안경찬은 천재인가, 바보인가";
+	
+		assertThat(testString.substring(0, 2), is("안경") );
+		
+	}
+	
 }
