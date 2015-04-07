316d69bdcfeb69efd171916b9eb3182c5dd5317e
Chris. Ahn <cgpink@hanmail.net>
2015-04-07 12:15:02 +0900

    TODO Task 처리

--- src/marshalling/MarshallingTest.java
+++ src/marshalling/MarshallingTest.java
@@ -10,10 +10,10 @@ public class MarshallingTest {
 	@Test
 	public void testMarshalling() {
 		
-		// JDK 1.6을 활용한 XML Marshalling 테스트 코드 작성
+		// TODO JDK 1.6을 활용한 XML Marshalling 테스트 코드 작성
 		
 		
-		// Spring 3.X를 활용한 XML Marshalling 테스트 코드 작성
+		// TODO Spring 3.X를 활용한 XML Marshalling 테스트 코드 작성
 		
 	}
 }
